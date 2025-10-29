3f94e05a973c3c032b50c16d6f262b356e171444
Mohathis <136694073+Mohathis@users.noreply.github.com>
2025-10-29 10:46:52 +0530

    completed

---
+++ record14b/graphics/circle.py
@@ -0,0 +1,5 @@
+def c_area(r):
+    print("Area of circle" ,3.14 * r * r)
+
+def c_perimeter(r):
+    print("Perimeter of circle", 2 * 3.14 * r)
---
+++ record14b/graphics/rectangle.py
@@ -0,0 +1,6 @@
+def r_area(l,b):
+    print("Area of rectangle",l * b)
+
+def r_perimeter(l,b):
+    print("perimeter of rectangle", 2 * (l + b))
+
